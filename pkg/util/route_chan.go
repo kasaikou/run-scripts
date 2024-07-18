867da82d7bdee39cc2ccc5d869b697784cd34feb
Kasai Kou <80010105+kasaikou@users.noreply.github.com>
2024-07-18 13:53:29 +0900

    docs: add comment

--- pkg/util/route_chan.go
+++ pkg/util/route_chan.go
@@ -2,6 +2,9 @@ package util
 
 import "context"
 
+// RouteChanContext monitors [context.Context] and reciever channel.
+//
+// It also loops until interrupted by ctx or reciever is closed.
 func RouteChanContext[T any](ctx context.Context, reciever <-chan T) func(yield func(val T) bool) {
 	return func(yield func(val T) bool) {
 		for {
